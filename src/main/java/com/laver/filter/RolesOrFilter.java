1fd72dc60a746f5fe8299f6cd53dcb8ee2e22cda
laver <447205752@qq.com>
2018-09-09 16:10:19 +0800

    RELEASE EDITION

--- src/main/java/com/laver/filter/RolesOrFilter.java
+++ src/main/java/com/laver/filter/RolesOrFilter.java
@@ -16,11 +16,13 @@ public class RolesOrFilter extends AuthorizationFilter {
         Subject subject=getSubject(servletRequest,servletResponse);
         // Object o 为spring.xml传过来的角色数组
         String[] roles = (String []) o;
+        //判断在配置文件中设置访问是否要经过角色校验
         if (roles == null || roles.length == 0){
             return true;
         }
         for (String role : roles){
             if (subject.hasRole(role)){
+                //有其中一个role即可返回
                 return true;
             }
         }
--- src/main/java/com/laver/shiro/realm/CustomRealm.java
+++ src/main/java/com/laver/shiro/realm/CustomRealm.java
@@ -89,6 +89,5 @@ public class CustomRealm extends AuthorizingRealm {
     public static void main(String[] args) {
         Md5Hash md5Hash=new Md5Hash("admin",salt);
         System.out.println(md5Hash);
-
     }
 }
